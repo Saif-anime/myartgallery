77d911f2e598c79ca875e47da44f05539cee8162
Saif-anime <saif817883@gmail.com>
2024-06-22 20:07:16 +0530

    Admin Kajogari

--- src/App.js
+++ src/App.js
@@ -36,15 +36,15 @@ function App() {
                   <div class="card info-card sales-card">
 
                     <div class="filter">
-                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <a class="icon" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                         <li class="dropdown-header text-start">
                           <h6>Filter</h6>
                         </li>
 
-                        <li><a class="dropdown-item" href="#">Today</a></li>
-                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                        <li><a class="dropdown-item">Today</a></li>
+                        <li><a class="dropdown-item" >This Month</a></li>
+                        <li><a class="dropdown-item" >This Year</a></li>
                       </ul>
                     </div>
 
@@ -69,15 +69,15 @@ function App() {
                   <div class="card info-card revenue-card">
 
                     <div class="filter">
-                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <a class="icon" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                         <li class="dropdown-header text-start">
                           <h6>Filter</h6>
                         </li>
 
-                        <li><a class="dropdown-item" href="#">Today</a></li>
-                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                        <li><a class="dropdown-item" >Today</a></li>
+                        <li><a class="dropdown-item" >This Month</a></li>
+                        <li><a class="dropdown-item" >This Year</a></li>
                       </ul>
                     </div>
 
@@ -103,15 +103,15 @@ function App() {
                   <div class="card info-card customers-card">
 
                     <div class="filter">
-                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <a class="icon"  data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                         <li class="dropdown-header text-start">
                           <h6>Filter</h6>
                         </li>
 
-                        <li><a class="dropdown-item" href="#">Today</a></li>
-                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                        <li><a class="dropdown-item" >Today</a></li>
+                        <li><a class="dropdown-item" >This Month</a></li>
+                        <li><a class="dropdown-item" >This Year</a></li>
                       </ul>
                     </div>
 
@@ -137,15 +137,15 @@ function App() {
                   <div class="card">
 
                     <div class="filter">
-                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <a class="icon"  data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                         <li class="dropdown-header text-start">
                           <h6>Filter</h6>
                         </li>
 
-                        <li><a class="dropdown-item" href="#">Today</a></li>
-                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                        <li><a class="dropdown-item" >Today</a></li>
+                        <li><a class="dropdown-item" >This Month</a></li>
+                        <li><a class="dropdown-item" >This Year</a></li>
                       </ul>
                     </div>
 
@@ -157,15 +157,15 @@ function App() {
                   <div class="card recent-sales overflow-auto">
 
                     <div class="filter">
-                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <a class="icon"  data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                         <li class="dropdown-header text-start">
                           <h6>Filter</h6>
                         </li>
 
-                        <li><a class="dropdown-item" href="#">Today</a></li>
-                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                        <li><a class="dropdown-item" >Today</a></li>
+                        <li><a class="dropdown-item" >This Month</a></li>
+                        <li><a class="dropdown-item" >This Year</a></li>
                       </ul>
                     </div>
 
@@ -184,37 +184,37 @@ function App() {
                         </thead>
                         <tbody>
                           <tr>
-                            <th scope="row"><a href="#">#2457</a></th>
+                            <th scope="row"><a >#2457</a></th>
                             <td>Brandon Jacob</td>
-                            <td><a href="#" class="text-primary">At praesentium minu</a></td>
+                            <td><a class="text-primary">At praesentium minu</a></td>
                             <td>$64</td>
                             <td><span class="badge bg-success">Approved</span></td>
                           </tr>
                           <tr>
-                            <th scope="row"><a href="#">#2147</a></th>
+                            <th scope="row"><a >#2147</a></th>
                             <td>Bridie Kessler</td>
-                            <td><a href="#" class="text-primary">Blanditiis dolor omnis similique</a></td>
+                            <td><a  class="text-primary">Blanditiis dolor omnis similique</a></td>
                             <td>$47</td>
                             <td><span class="badge bg-warning">Pending</span></td>
                           </tr>
                           <tr>
-                            <th scope="row"><a href="#">#2049</a></th>
+                            <th scope="row"><a >#2049</a></th>
                             <td>Ashleigh Langosh</td>
-                            <td><a href="#" class="text-primary">At recusandae consectetur</a></td>
+                            <td><a class="text-primary">At recusandae consectetur</a></td>
                             <td>$147</td>
                             <td><span class="badge bg-success">Approved</span></td>
                           </tr>
                           <tr>
-                            <th scope="row"><a href="#">#2644</a></th>
+                            <th scope="row"><a >#2644</a></th>
                             <td>Angus Grady</td>
-                            <td><a href="#" class="text-primar">Ut voluptatem id earum et</a></td>
+                            <td><a  class="text-primar">Ut voluptatem id earum et</a></td>
                             <td>$67</td>
                             <td><span class="badge bg-danger">Rejected</span></td>
                           </tr>
                           <tr>
-                            <th scope="row"><a href="#">#2644</a></th>
+                            <th scope="row"><a >#2644</a></th>
                             <td>Raheem Lehner</td>
-                            <td><a href="#" class="text-primary">Sunt similique distinctio</a></td>
+                            <td><a  class="text-primary">Sunt similique distinctio</a></td>
                             <td>$165</td>
                             <td><span class="badge bg-success">Approved</span></td>
                           </tr>
@@ -229,15 +229,15 @@ function App() {
                   <div class="card top-selling overflow-auto">
 
                     <div class="filter">
-                      <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                      <a class="icon"  data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                         <li class="dropdown-header text-start">
                           <h6>Filter</h6>
                         </li>
 
-                        <li><a class="dropdown-item" href="#">Today</a></li>
-                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                        <li><a class="dropdown-item" href="#">This Year</a></li>
+                        <li><a class="dropdown-item" >Today</a></li>
+                        <li><a class="dropdown-item" >This Month</a></li>
+                        <li><a class="dropdown-item" >This Year</a></li>
                       </ul>
                     </div>
 
@@ -256,36 +256,36 @@ function App() {
                         </thead>
                         <tbody>
                           <tr>
-                            <th scope="row"><a href="#"><img src="assets/img/product-1.jpg" alt="" /></a></th>
-                            <td><a href="#" class="text-primary fw-bold">Ut inventore ipsa voluptas nulla</a></td>
+                            <th scope="row"><a ><img src="assets/img/product-1.jpg" alt="" /></a></th>
+                            <td><a  class="text-primary fw-bold">Ut inventore ipsa voluptas nulla</a></td>
                             <td>$64</td>
                             <td class="fw-bold">124</td>
                             <td>$5,828</td>
                           </tr>
                           <tr>
-                            <th scope="row"><a href="#"><img src="assets/img/product-2.jpg" alt="" /></a></th>
-                            <td><a href="#" class="text-primary fw-bold">Exercitationem similique doloremque</a></td>
+                            <th scope="row"><a ><img src="assets/img/product-2.jpg" alt="" /></a></th>
+                            <td><a  class="text-primary fw-bold">Exercitationem similique doloremque</a></td>
                             <td>$46</td>
                             <td class="fw-bold">98</td>
                             <td>$4,508</td>
                           </tr>
                           <tr>
-                            <th scope="row"><a href="#"><img src="assets/img/product-3.jpg" alt="" /></a></th>
-                            <td><a href="#" class="text-primary fw-bold">Doloribus nisi exercitationem</a></td>
+                            <th scope="row"><a ><img src="assets/img/product-3.jpg" alt="" /></a></th>
+                            <td><a  class="text-primary fw-bold">Doloribus nisi exercitationem</a></td>
                             <td>$59</td>
                             <td class="fw-bold">74</td>
                             <td>$4,366</td>
                           </tr>
                           <tr>
-                            <th scope="row"><a href="#"><img src="assets/img/product-4.jpg" alt="" /></a></th>
-                            <td><a href="#" class="text-primary fw-bold">Officiis quaerat sint rerum error</a></td>
+                            <th scope="row"><a ><img src="assets/img/product-4.jpg" alt="" /></a></th>
+                            <td><a  class="text-primary fw-bold">Officiis quaerat sint rerum error</a></td>
                             <td>$32</td>
                             <td class="fw-bold">63</td>
                             <td>$2,016</td>
                           </tr>
                           <tr>
-                            <th scope="row"><a href="#"><img src="assets/img/product-5.jpg" alt="" /></a></th>
-                            <td><a href="#" class="text-primary fw-bold">Sit unde debitis delectus repellendus</a></td>
+                            <th scope="row"><a ><img src="assets/img/product-5.jpg" alt="" /></a></th>
+                            <td><a  class="text-primary fw-bold">Sit unde debitis delectus repellendus</a></td>
                             <td>$79</td>
                             <td class="fw-bold">41</td>
                             <td>$3,239</td>
